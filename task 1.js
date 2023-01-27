91332db9f5aea9c9b26d47b959ff23490c9ce708
Niiikhiiil <123645218+Niiikhiiil@users.noreply.github.com>
2023-01-27 17:25:38 +0530

    Add files via upload

---
+++ task 1.js
@@ -0,0 +1,12 @@
+//task 1
+//create a function to filter array of numbers
+// in filter method ,there is always a condition that filter the array 
+//or we can use map to iterate the elements of array
+const arr=[-2,-8,5,6,7,1,9,0,10,-1] 
+const filterhandle=()=>{
+	const a=arr.filter(ele=>ele>=6)
+	return a;
+}
+console.log(filterhandle());
+
+// output:[ 6, 7, 9, 10 ]
